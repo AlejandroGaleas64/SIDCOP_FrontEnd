068fbc6ae2fe2bc3605bca982ec7bf7b8cad134f
Stephtor29 <gisselletoro22@gmail.com>
2025-08-14 08:21:02 -0600

    Update Reportes

--- Admin/src/app/pages/reportes/clientesPorCanalYFecha/list/list.component.ts
+++ Admin/src/app/pages/reportes/clientesPorCanalYFecha/list/list.component.ts
@@ -6,11 +6,12 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { PdfReportService, ReportConfig, TableData } from 'src/app/reporteGlobal';
+import { BreadcrumbsComponent } from 'src/app/shared/breadcrumbs/breadcrumbs.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, BreadcrumbsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
