ecb9a1f9a7191fd74c9b106a90790268ba9a771c
WILCRACKGAMER <wilcrack2006@gmail.com>
2025-08-19 13:39:43 -0600

    devoluciones

--- Admin/src/app/Modelos/ventas/DevolucionesDetalle.Model.ts
+++ Admin/src/app/Modelos/ventas/DevolucionesDetalle.Model.ts
@@ -6,13 +6,14 @@ export class DevolucionesDetalle {
     prod_Descripcion: string = '';
     prod_DescripcionCorta: string = '';
     cate_Descripcion: string = '';
+    subc_Descripcion: string = '';
     marc_Descripcion: string = '';
-    prod_Imagen: string = '';
+    prod_Imagen?: string = '';
     marc_Id: number = 0;
     usu_Creacion: string = '';
-    devD_FechaCreacion: string = '';
-    usu_Modificacion: string = '';
-    devD_FechaModificacion: string = '';
+    devD_FechaCreacion: Date = new Date();
+    usu_Modificacion?: string = '';
+    devD_FechaModificacion?: Date = new Date();
     devD_Estado: boolean = true;
 
     constructor(init?: Partial<DevolucionesDetalle>) {
