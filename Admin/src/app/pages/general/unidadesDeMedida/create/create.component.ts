ed28362cd7297d002d09864886b666da3f220332
Stephtor29 <gisselletoro22@gmail.com>
2025-08-26 23:28:22 -0600

    CRUD UNIDADES DE PESO

---
+++ Admin/src/app/pages/general/unidadesDeMedida/create/create.component.ts
@@ -0,0 +1,154 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UnidadDePeso } from 'src/app/Modelos/general/UnidadDePeso.Model';
+import { environment } from 'src/environments/environment.prod';
+import { getUserId } from 'src/app/core/utils/user-utils';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [CommonModule, FormsModule, HttpClientModule],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss'
+})
+export class CreateComponent {
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSave = new EventEmitter<UnidadDePeso>();
+  @Output() onOverlayChange = new EventEmitter<boolean>();
+
+  mostrarErrores = false;
+  mostrarAlertaExito = false;
+  mensajeExito = '';
+  mostrarAlertaError = false;
+  mensajeError = '';
+  mostrarAlertaWarning = false;
+  mensajeWarning = '';
+
+  constructor(private http: HttpClient) {}
+
+  UnidadDePeso: UnidadDePeso = {
+    unPe_Id: 0,
+    unPe_Descripcion: '',
+    usua_Creacion: 0,
+    usua_Modificacion: 0,
+    secuencia: 0,
+    unPe_FechaCreacion: new Date(),
+    unPe_FechaModificacion: new Date(),
+    code_Status: 0,
+    message_Status: '',
+    usuarioCreacion: '',
+    usuarioModificacion: ''
+  };
+
+  cancelar(): void {
+    this.mostrarErrores = false;
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+    this.onOverlayChange.emit(false);
+    this.UnidadDePeso = {
+      unPe_Id: 0,
+      unPe_Descripcion: '',
+      usua_Creacion: 0,
+      usua_Modificacion: 0,
+      secuencia: 0,
+      unPe_FechaCreacion: new Date(),
+      unPe_FechaModificacion: new Date(),
+      code_Status: 0,
+      message_Status: '',
+      usuarioCreacion: '',
+      usuarioModificacion: ''
+    };
+    this.onCancel.emit();
+  }
+
+  cerrarAlerta(): void {
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+  }
+
+  guardar(): void {
+    this.mostrarErrores = true;
+    this.onOverlayChange.emit(true);
+    if (this.UnidadDePeso.unPe_Descripcion.trim()) {
+      // Limpiar alertas previas
+      this.mostrarAlertaWarning = false;
+      this.mostrarAlertaError = false;
+      
+      const UnidadDePesoGuardar = {
+        unPe_Id: 0,
+        unPe_Descripcion: this.UnidadDePeso.unPe_Descripcion.trim(),
+        usua_Creacion: getUserId(), // variable global, obtiene el valor del environment, esto por mientras
+        unPe_FechaCreacion: new Date().toISOString(),
+        usua_Modificacion: 0,
+        numero: "", 
+        unPe_FechaModificacion: new Date().toISOString(),
+        usuarioCreacion: "", 
+        usuarioModificacion: "" 
+      };
+
+      console.log('Guardando Unidad De Peso:', UnidadDePesoGuardar);
+      
+      this.http.post<any>(`${environment.apiBaseUrl}/UnidadesMedida/Insertar`, UnidadDePesoGuardar, {
+        headers: { 
+          'X-Api-Key': environment.apiKey,
+          'Content-Type': 'application/json',
+          'accept': '*/*'
+        }
+      }).subscribe({
+        next: (response) => {
+          console.log('Unidad De Peso guardada exitosamente:', response);
+          this.mostrarErrores = false;
+          setTimeout(() => {
+            this.onOverlayChange.emit(false);
+            this.mensajeExito = `Unidad De Peso "${this.UnidadDePeso.unPe_Descripcion}" guardada exitosamente`;
+            this.mostrarAlertaExito = true;
+            setTimeout(() => {
+              this.mostrarAlertaExito = false;
+              setTimeout(() => {
+                this.onSave.emit(this.UnidadDePeso);
+                this.cancelar();
+              }, 100);
+            }, 2000);
+          }, 300);
+        },
+        error: (error) => {
+          setTimeout(() => {
+            this.onOverlayChange.emit(false);
+            console.error('Error al guardar Unidad De Peso:', error);
+            this.mostrarAlertaError = true;
+            this.mensajeError = 'Error al guardar la Unidad De Peso Por favor, intente nuevamente.';
+            this.mostrarAlertaExito = false;
+            // Ocultar la alerta de error después de 5 segundos
+            setTimeout(() => {
+              this.mostrarAlertaError = false;
+              this.mensajeError = '';
+            }, 5000);
+          }, 1000);
+        }
+      });
+    } else {
+      // Mostrar alerta de warning para campos vacíos
+      this.onOverlayChange.emit(false);
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      this.mostrarAlertaError = false;
+      this.mostrarAlertaExito = false;
+      
+      // Ocultar la alerta de warning después de 4 segundos
+      setTimeout(() => {
+        this.mostrarAlertaWarning = false;
+        this.mensajeWarning = '';
+      }, 4000);
+    }
+  }
+}
--- Admin/src/app/pages/general/unidadesDeMedida/edit/edit.component.ts
+++ Admin/src/app/pages/general/unidadesDeMedida/edit/edit.component.ts
@@ -1,12 +1,167 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { UnidadDePeso } from 'src/app/Modelos/general/UnidadDePeso.Model';
+import { environment } from 'src/environments/environment.prod';
+import { getUserId } from 'src/app/core/utils/user-utils';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
-export class EditComponent {
+export class EditComponent implements OnChanges {
+  // Devuelve la lista de cambios detectados para el modal de confirmación
+  obtenerListaCambios() {
+    const cambios = [];
+    if (this.UnidadDePeso.unPe_Descripcion?.trim() !== this.UnidadDePesoOriginal?.trim()) {
+      cambios.push({
+        label: 'Descripción',
+        anterior: this.UnidadDePesoOriginal,
+        nuevo: this.UnidadDePeso.unPe_Descripcion
+      });
+    }
+    return cambios;
+  }
+  
+  @Input() UnidadDePesoData: UnidadDePeso | null = null;
+  @Output() onCancel = new EventEmitter<void>();
+  @Output() onSave = new EventEmitter<UnidadDePeso>();
+  @Output() onOverlayChange = new EventEmitter<boolean>();
 
+  UnidadDePeso: UnidadDePeso = {
+    unPe_Id: 0,
+    unPe_Descripcion: '',
+    usua_Creacion: 0,
+    usua_Modificacion: 0,
+    secuencia: 0,
+    unPe_FechaCreacion: new Date(),
+    unPe_FechaModificacion: new Date(),
+    code_Status: 0,
+    message_Status: '',
+    usuarioCreacion: '',
+    usuarioModificacion: ''
+  };
+
+  UnidadDePesoOriginal = '';
+  mostrarErrores = false;
+  mostrarAlertaExito = false;
+  mensajeExito = '';
+  mostrarAlertaError = false;
+  mensajeError = '';
+  mostrarAlertaWarning = false;
+  mensajeWarning = '';
+  mostrarConfirmacionEditar = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['UnidadDePesoData'] && changes['UnidadDePesoData'].currentValue) {
+      this.UnidadDePeso = { ...changes['UnidadDePesoData'].currentValue };
+      this.UnidadDePesoOriginal = this.UnidadDePeso.unPe_Descripcion || '';
+      this.mostrarErrores = false;
+      this.cerrarAlerta();
+    }
+  }
+
+  cancelar(): void {
+    this.cerrarAlerta();
+    this.onCancel.emit();
+  }
+
+  cerrarAlerta(): void {
+    this.mostrarAlertaExito = false;
+    this.mensajeExito = '';
+    this.mostrarAlertaError = false;
+    this.mensajeError = '';
+    this.mostrarAlertaWarning = false;
+    this.mensajeWarning = '';
+  }
+
+  validarEdicion(): void {
+    this.mostrarErrores = true;
+
+    if (this.UnidadDePeso.unPe_Descripcion.trim()) {
+      if (this.UnidadDePeso.unPe_Descripcion.trim() !== this.UnidadDePesoOriginal) {
+        this.mostrarConfirmacionEditar = true;
+      } else {
+        this.mostrarAlertaWarning = true;
+        this.mensajeWarning = 'No se han detectado cambios.';
+        setTimeout(() => this.cerrarAlerta(), 4000);
+      }
+    } else {
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      setTimeout(() => this.cerrarAlerta(), 4000);
+    }
+  }
+
+  cancelarEdicion(): void {
+    this.mostrarConfirmacionEditar = false;
+  }
+
+  confirmarEdicion(): void {
+    this.mostrarConfirmacionEditar = false;
+    this.guardar();
+  }
+
+  private guardar(): void {
+    this.mostrarErrores = true;
+    this.onOverlayChange.emit(true);
+
+    if (this.UnidadDePeso.unPe_Descripcion.trim()) {
+      const UnidadDePesoActualizar = {
+        unPe_Id: this.UnidadDePeso.unPe_Id,
+        unPe_Descripcion: this.UnidadDePeso.unPe_Descripcion.trim(),
+        usua_Creacion: this.UnidadDePeso.usua_Creacion,
+        unPe_FechaCreacion: this.UnidadDePeso.unPe_FechaCreacion,
+        usua_Modificacion: getUserId(),
+        numero: this.UnidadDePeso.secuencia || '',
+        unPe_FechaModificacion: new Date().toISOString(),
+        usuarioCreacion: '',
+        usuarioModificacion: ''
+      };
+
+      this.http.put<any>(`${environment.apiBaseUrl}/UnidadesMedida/Actualizar`, UnidadDePesoActualizar, {
+        headers: {
+          'X-Api-Key': environment.apiKey,
+          'Content-Type': 'application/json',
+          'accept': '*/*'
+        }
+      }).subscribe({
+        next: (response) => {
+          this.mostrarErrores = false;
+          setTimeout(() => {
+            this.onOverlayChange.emit(false);
+            this.mensajeExito = `Unidad De Peso "${this.UnidadDePeso.unPe_Descripcion}" actualizada exitosamente`;
+            this.mostrarAlertaExito = true;
+            setTimeout(() => {
+              this.mostrarAlertaExito = false;
+              setTimeout(() => {
+                this.onSave.emit(this.UnidadDePeso);
+                this.cancelar();
+              }, 100);
+            }, 2000);
+          }, 300);
+        },
+        error: (error) => {
+          setTimeout(() => {
+            this.onOverlayChange.emit(false);
+            console.error('Error al actualizar Unidad De Peso:', error);
+            this.mostrarAlertaError = true;
+            this.mensajeError = 'Error al actualizar la Unidad De Peso. Por favor, intente nuevamente.';
+            setTimeout(() => this.cerrarAlerta(), 5000);
+          }, 1000);
+        }
+      });
+    } else {
+      this.onOverlayChange.emit(false);
+      this.mostrarAlertaWarning = true;
+      this.mensajeWarning = 'Por favor complete todos los campos requeridos antes de guardar.';
+      setTimeout(() => this.cerrarAlerta(), 4000);
+    }
+  }
 }
