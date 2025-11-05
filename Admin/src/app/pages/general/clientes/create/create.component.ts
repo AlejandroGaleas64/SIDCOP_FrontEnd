8c2b017a59721a44a37ad1461be3dac34972ff8b
Helen Euceda <neuceda13@gmail.com>
2025-11-05 15:15:42 -0600

    Listo

--- Admin/src/app/pages/general/clientes/create/create.component.ts
+++ Admin/src/app/pages/general/clientes/create/create.component.ts
@@ -31,7 +31,7 @@ export class CreateComponent {
   mapaSelectorComponent!: MapaSelectorComponent;
 
   entrando = true;
-  activeTab = 3;
+  activeTab = 1;
 
   mostrarErrores = false;
   mostrarAlertaExito = false;
